c93f269f262fb5ec6cdcd53e5aedc362157cabc3
Justice Adam <justiceadam673@gmail.com>
2025-08-04 15:34:17 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
 function App() {
   return (
     <>
-      <h1 className='bg-red-400 px-2- py-10 rounded-2xl '>
-        hello every dev, please work should begin as soon as possible!!!!!!
-      </h1>
+      <main className='bg-red-100 text-red-500 flex justify-center items-center flex-col gap-10 w-screen h-screen px-2- py-10  '>
+        <Icon
+          icon={"eos-icons:bubble-loading"}
+          width={200}
+          height={200}
+          className='text-blue-900 '
+        />
+        <h1 className=' animate-bounce transition-all duration-1000 text-3xl font-bold text-center'>
+          <span className='text-yellow-400'>hello</span>{" "}
+          <span className='text-orange-500'>fellow</span>{" "}
+          <span className='text-purple-700'>dev</span>,{" "}
+          <span className=' text-pink-600 '>welcome</span>{" "}
+          <span className=' text-cyan-400 '>to</span>{" "}
+          <span className=' text-green-500 '>HOMETRACE</span>
+          <span className=' text-gray-500 '>!!!!!!</span>
+        </h1>
+      </main>
     </>
   );
 }
