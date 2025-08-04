c0691fe178c4f70777333fd1df1d3624aa76f0bf
Justice Adam <justiceadam673@gmail.com>
2025-08-04 14:55:28 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className='bg-red-400 px-2- py-10 rounded-2xl '>
+        hello every dev, please work should begin as soon as possible!!!!!!
+      </h1>
+    </>
+  );
+}
+
+export default App;
