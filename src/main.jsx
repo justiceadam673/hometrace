723d40814c79a572b34fa7a0b0c682a6a1ef6e63
Justice Adam <justiceadam673@gmail.com>
2025-08-05 10:51:19 +0100

    made to pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')).render(
+registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      window.location.reload();
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Hometrace",
+        short_name: "HTC",
+        description:
+          "Hometrace is a property tech app helping people find houses or land to rent or buy, and connect with trusted agents.",
+        theme_color: "#ffffff",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png", // ✅ fixed: no "/public"
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
